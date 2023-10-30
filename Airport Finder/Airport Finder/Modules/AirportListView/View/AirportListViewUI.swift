d47ef03143ac6014b13b42cb6a9d20505f28e6a7
Narciso Meza <ncismeba@gmail.com>
2023-10-30 08:09:27 -0600

    Update AirportListViewUI.swift
    Se agregan las variables viewSelection y manager con la intención de poder enfocar el marcador seleccionado.

--- Airport Finder/Airport Finder/Modules/AirportListView/View/AirportListViewUI.swift
+++ Airport Finder/Airport Finder/Modules/AirportListView/View/AirportListViewUI.swift
@@ -6,13 +6,21 @@ import SwiftUI
 import MapKit
 
 struct AirportListViewUI: View {
+	@Binding var viewSelection: Int
 	@Binding var annotations: [Location]
+	@Binding var manager: LocationManager
 	
     var body: some View {
 		VStack {
 			List {
 				ForEach(annotations, id: \.name) { airport in
 					Text(airport.name)
+						.onTapGesture {
+							viewSelection = 0
+							let region = MKCoordinateRegion(center: airport.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+							$manager.region = region
+
+						}
 				}
 			}
 			.padding(.top)
@@ -23,5 +31,5 @@ struct AirportListViewUI: View {
 }
 
 #Preview {
-	AirportListViewUI(annotations: .constant([Location(name: "1", coordinate: CLLocationCoordinate2D(latitude: 20.703444, longitude: -101.368224))]))
+	AirportListViewUI(annotations: .constant([Location(name: "1", coordinate: CLLocationCoordinate2D(latitude: 20.703444, longitude: -101.368224))]), viewSelection: .constant(0))
 }
