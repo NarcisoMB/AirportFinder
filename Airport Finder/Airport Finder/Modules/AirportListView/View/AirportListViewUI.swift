e7b0f528072f1d080cd9aa2b8774262e5f966c50
Narciso Meza <ncismeba@gmail.com>
2023-10-30 08:47:20 -0600

    Update AirportListViewUI.swift
    Se crea Constants para hacer el manejo de textos mas sencillo.

--- Airport Finder/Airport Finder/Modules/AirportListView/View/AirportListViewUI.swift
+++ Airport Finder/Airport Finder/Modules/AirportListView/View/AirportListViewUI.swift
@@ -5,10 +5,16 @@
 import SwiftUI
 import MapKit
 
+fileprivate struct Constants {
+	static var navTitle = "Airport list near you"
+}
+
 struct AirportListViewUI: View {
 	@Binding var viewSelection: Int
 	@Binding var annotations: [Location]
 	@Binding var manager: LocationManager
+	
+	var hostView: HostViewUI
 	
     var body: some View {
 		VStack {
@@ -16,20 +22,20 @@ struct AirportListViewUI: View {
 				ForEach(annotations, id: \.name) { airport in
 					Text(airport.name)
 						.onTapGesture {
+							let manager = LocationManager()
+							manager.region = MKCoordinateRegion(center: airport.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+							hostView.focusAnnotation(manager)
 							viewSelection = 0
-//							let region = MKCoordinateRegion(center: airport.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-//							$manager.region = region
-
 						}
 				}
 			}
 			.padding(.top)
 		}
 		.navigationBarBackButtonHidden(true)
-		.navigationTitle("Airport list near you")
+		.navigationTitle(Constants.navTitle)
     }
 }
 
 #Preview {
-	AirportListViewUI(viewSelection: .constant(0), annotations: .constant([Location(name: "1", coordinate: CLLocationCoordinate2D(latitude: 20.703444, longitude: -101.368224))]), manager: .constant(LocationManager()))
+	AirportListViewUI(viewSelection: .constant(0), annotations: .constant([Location(name: "1", coordinate: CLLocationCoordinate2D(latitude: 20.703444, longitude: -101.368224))]), manager: .constant(LocationManager()), hostView: HostViewUI(distance: .constant(1)))
 }
