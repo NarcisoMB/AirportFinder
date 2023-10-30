3527788e7dc61b1dbb54643b46b311f8c351579e
Narciso Meza <ncismeba@gmail.com>
2023-10-30 09:32:58 -0600

    Update MapViewUI.swift
    Se agrega la vista de HostViewUI como parámetro a recibir y se agrega la Constant de location

--- Airport Finder/Airport Finder/Modules/MapView/View/MapViewUI.swift
+++ Airport Finder/Airport Finder/Modules/MapView/View/MapViewUI.swift
@@ -5,9 +5,15 @@
 import SwiftUI
 import MapKit
 
-struct MapViewUI: View {	
+fileprivate struct Constants {
+	static var location = "location"
+}
+
+struct MapViewUI: View {
 	@Binding var manager: LocationManager
 	@Binding var annotations: [Location]
+	
+	var hostView: HostViewUI
 	
 	var body: some View {
 		Map(coordinateRegion: $manager.region,
@@ -23,5 +29,5 @@ struct MapViewUI: View {
 }
 
 #Preview {
-	MapViewUI(manager: .constant(LocationManager()), annotations: .constant([Location(name: "1", coordinate: CLLocationCoordinate2D(latitude: 20.703444, longitude: -101.368224))]))
+	MapViewUI(manager: .constant(LocationManager()), annotations: .constant([Location(name: "1", coordinate: CLLocationCoordinate2D(latitude: 20.703444, longitude: -101.368224))]), hostView: HostViewUI(distance: .constant(1)))
 }
