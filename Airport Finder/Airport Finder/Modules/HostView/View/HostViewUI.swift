c7830c17c6c9d3c09af0334218ed685f79fe2e64
Narciso Meza <ncismeba@gmail.com>
2023-10-30 09:32:21 -0600

    Update HostViewUI.swift
    Se agrega botón para enfocar la ubicación del usuario, se agrega lógica para presentar el primer valor recibido de la respuesta, siendo este el resultado mas cercano a la ubicación del usuario y de igualmente se manda la vista (HostViewUI) a MapViewUI.

--- Airport Finder/Airport Finder/Modules/HostView/View/HostViewUI.swift
+++ Airport Finder/Airport Finder/Modules/HostView/View/HostViewUI.swift
@@ -7,6 +7,7 @@ import MapKit
 
 fileprivate struct Constants {
 	static var map = "map.circle"
+	static var location = "location"
 	static var airport = "airplane.circle"
 	static var coordinates = LocationManager().locationManager.location?.coordinate
 }
@@ -16,7 +17,12 @@ struct HostViewUI: View {
 	@EnvironmentObject private var aviationDataGateway: AviationDataGateway
 	
 	@State private var annotations: [Location] = []
-	@State private var manager = LocationManager()
+	@State private var manager = LocationManager() {
+		didSet {
+			print(manager.region)
+		}
+	}
+	
 	@State private var viewSelection = 0
 	
 	@Binding var distance: Double
@@ -24,12 +30,38 @@ struct HostViewUI: View {
 	var body: some View {
 		ZStack {
 			if viewSelection == 0 {
-				MapViewUI(manager: $manager, annotations: $annotations)
+				MapViewUI(manager: $manager, annotations: $annotations, hostView: self)
 			} else if viewSelection == 1 {
 				AirportListViewUI(viewSelection: $viewSelection, annotations: $annotations, manager: $manager, hostView: self)
 			}
 			VStack {
 				Spacer()
+				HStack {
+					Spacer()
+					Button(action: {
+						withAnimation {
+							let manager = LocationManager()
+							if let location = manager.locationManager.location {
+								manager.region = MKCoordinateRegion(
+									center: location.coordinate,
+									span: MKCoordinateSpan(
+										latitudeDelta: 1,
+										longitudeDelta: 1
+									)
+								)
+								focusAnnotation(manager)
+							}
+						}
+					}, label: {
+						Image(systemName: Constants.location)
+							.background() {
+								Circle()
+									.fill(.gray)
+									.frame(width: 32, height: 32)
+							}
+					})
+					.padding([.bottom, .trailing])
+				}
 				Picker("", selection: $viewSelection) {
 					Image(systemName: Constants.map).tag(0)
 					Image(systemName: Constants.airport).tag(1)
@@ -45,14 +77,27 @@ struct HostViewUI: View {
 			}
 		}
 		.task {
-			if let lat = Constants.coordinates?.latitude, let lng = Constants.coordinates?.longitude {
-				let dto = AviationDataRequest(
-					lat: "\(Int(lat))",
-					lon: "\(Int(lng))",
-					radius: "\(Int(distance))"
-				)
-				aviationDataGateway.nearbyAirports(dto) { result in
-					annotations = result.map{ $0.toLocation() }
+			withAnimation {
+				if let lat = Constants.coordinates?.latitude, let lng = Constants.coordinates?.longitude {
+					let dto = AviationDataRequest(
+						lat: "\(Int(lat))",
+						lon: "\(Int(lng))",
+						radius: "\(Int(distance))"
+					)
+					aviationDataGateway.nearbyAirports(dto) { result in
+						annotations = result.map{ $0.toLocation() }
+						if let coordinate = annotations.first?.coordinate {
+							let manager = LocationManager()
+							manager.region = MKCoordinateRegion(
+								center: coordinate,
+								span: MKCoordinateSpan(
+									latitudeDelta: 1,
+									longitudeDelta: 1
+								)
+							)
+							focusAnnotation(manager)
+						}
+					}
 				}
 			}
 		}
